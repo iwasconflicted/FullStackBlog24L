96da7850f22bfd288802e0379f66d63c00157ce4
Saroeut Sim <saroeuts123@gmail.com>
2024-07-30 20:08:25 -0700

    DB context, started controllers 7-30-2024

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Models.DTO;
+using API.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -10,6 +12,38 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class UserController : ControllerBase
     {
-        
+        private readonly UserService _data;
+
+    public UserController(UserService datafromService)
+    {
+        _data = datafromService;
+    }
+
+// Add a user
+
+[HttpPost]
+
+public bool AddUser(CreateAccountDTO UserToAdd)
+{
+    return _data.AddUser(UserToAdd);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Login
+// Update user Account
+// Delete user Account
+
     }
 }
--- API/Services/UserService.cs
+++ API/Services/UserService.cs
@@ -1,12 +1,42 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
+using API.Models.DTO;
+using API.Services.Context;
 
 namespace API.Services
 {
     public class UserService
     {
-        
+        private readonly DataContext _context; 
+
+        public UserService(DataContext context)
+        {
+            _context = context;
+        }
+
+
+        // helper functions to help us check if the user exist
+        // DoesUserExist
+        public bool DoesUserExist(string username)
+        {
+            // check our tables to see if the user name exist
+            // if one item matches our condition, that item will be returned
+            // if no item matches, it'll return null
+            // if multiple items match, it will return error
+        }
+
+
+
+        // adding user logic
+
+        public bool AddUser(CreateAccountDTO userToAdd)
+        {
+        //    if the user already exist
+        // if the do not exist we add an account
+        // else throw a false
+        }
     }
 }
